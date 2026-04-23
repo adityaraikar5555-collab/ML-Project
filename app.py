c4a37f0cddadddf0e9d53413458142c716e7f98f
Aditya Raikar <adityaraikar5555@gmail.com>
2026-04-23 21:20:35 +0530

    fixed deloyment issues

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import pandas as pd
 
+from src.pipeline.predict_pipeline import PredictPipeline
+
 app = Flask(__name__)
 
 
@@ -13,8 +15,6 @@ def index():
 def predict_datapoint():
     if request.method == "GET":
         return render_template("home.html")
-
-    from src.pipeline.predict_pipeline import PredictPipeline
 
     data = pd.DataFrame([{
         "gender": request.form.get("gender"),
